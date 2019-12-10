cccf35e952e3ba794662df79da72548033629b46
João Amador <nnettoolliveira@gmail.com>
2019-12-09 23:49:28 -0300

    fazendo teste

--- cadastrar-cliente.db.php
+++ cadastrar-cliente.db.php
@@ -1,7 +1,8 @@
 <?php include('db.php') ?>
 
-<?php 
+<?php
 
+// adicionar o cliente na base de dados
 if (isset($_POST['adicionar'])) {
     $cliente = mysqli_real_escape_string($con, $_POST['cliente']);
     $sexo = mysqli_real_escape_string($con, $_POST['sexo']);
