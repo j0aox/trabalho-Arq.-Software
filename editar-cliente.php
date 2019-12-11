52883d95d62922601ce6b31106374cdbb4c20170
João Amador <nnettoolliveira@gmail.com>
2019-12-11 12:03:48 -0300

    corrigindo a rota do botão editar

--- editar-cliente.php
+++ editar-cliente.php
@@ -61,7 +61,7 @@ endif;
                         <td><?php echo $row['nome']; ?></td>
                         <td><?php echo $row['sexo']; ?></td>
                         <td>
-                            <a href="editar.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Editar</a>
+                            <a href="editar-cliente.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Editar</a>
                             <a href="cadastrar-cliente.php?excluir= <?php echo $row['id']; ?> " class="btn btn-danger">Excluir</a>
                         </td>
                     </tr>
