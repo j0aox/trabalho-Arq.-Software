c07337c5048d3a64399d5625dd7badc259b4bd69
João Amador <nnettoolliveira@gmail.com>
2019-12-12 11:20:18 -0300

    alguel do carro

---
+++ aluguel-carro.php
@@ -0,0 +1,62 @@
+<?php include('header.php') ?>
+
+<?php include('navbar.php') ?>
+
+<?php include('cadastrar-cliente.db.php') ?>
+
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <h3 class="text-center text-dark">Cadastrar Clientes</h3>
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h3 class="text-center text-secondary">Adicionar Cliente</h3>
+            <form action="" method="post" enctype="multipart/form-data">
+                <select class="form-control">
+                    <option>carro</option>
+                </select>
+                <br>
+                <select class="form-control">
+                    <option>cliente</option>
+                    <br>
+                </select>
+                <br>
+                <button type="submit" name="" class="btn btn-secondary btn-block">Adicionar</button>
+                <br>
+            </form>
+        </div>
+        <div class="col-md-8">
+        <?php 
+            //$sql = mysqli_query($con, "SELECT * FROM cliente");
+        ?>
+            <h3 class="text-center text-secondary">Lista de Clientes</h3>
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Carro</th>
+                        <th>Cliente</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php //while($row = mysqli_fetch_assoc($sql)){ ?>
+                    <tr>
+                        <td><?php // echo $row['nome']; ?></td>
+                        <td><?php // echo $row['sexo']; ?></td>
+                        <td>
+                            <a href="editar-cliente.php?id=<?php // echo $row['id']; ?>" class="btn btn-success">Editar</a>
+                            <a href="cadastrar-cliente.php?excluir= <?php // echo $row['id']; ?> " class="btn btn-danger">Excluir</a>
+                        </td>
+                    </tr>
+                <?php //} ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php') ?>
