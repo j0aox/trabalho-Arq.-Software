72e942602703be906f3030d27b37360df57029f1
João Amador <nnettoolliveira@gmail.com>
2019-12-12 10:07:03 -0300

    deixando o código limpo

--- dashboard.php
+++ dashboard.php
@@ -41,7 +41,7 @@
     <div id="piechart" style="width: 900px; height: 500px;"></div>
 
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+   
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
