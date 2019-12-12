cf1c798884ccca6b0e0015a8d143501bd8b55e90
João Amador <nnettoolliveira@gmail.com>
2019-12-12 10:00:38 -0300

    criando gráficos para relatório

--- dashboard.php
+++ dashboard.php
@@ -6,3 +6,62 @@
 
 
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    include ('db.php');
+
+    $sql = "SELECT * FROM cliente";
+    $resultado = mysqli_query($con, $sql);
+
+    $masculino = 0;
+    $feminino = 0;
+
+    while ($linha = mysqli_fetch_assoc($resultado))
+    {
+        if ($linha['sexo'] == 'M')
+            $masculino++;
+        else if ($linha['sexo'] == 'F')
+            $feminino++;
+    }
+
+    ?>
+    
+
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['SExo', 'Quantidade'],
+          ['Masculino',     <?php echo $masculino; ?>],
+          ['Feminino',      <?php echo $feminino; ?>]
+        ]);
+
+        var options = {
+          title: 'Gráfico por sexo'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+</body>
+</html>
